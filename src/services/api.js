8c28ee4180d47505caf44de2d8fe4a47d64e71de
SilasEduardo <silasandrade94@gmail.com>
2025-12-22 17:56:59 -0300

    Deploy

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://backend-mlst.onrender.com/api';
+const API_URL = 'https://backend-mlst-1.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_URL,
